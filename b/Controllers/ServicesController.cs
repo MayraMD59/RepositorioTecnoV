3af89b781ac1b80da9f2503ecd4dcd2ce035b4d7
Mayra Montoya <mayramontoyamd@gmail.com>
2020-04-16 03:41:31 -0500

    TeCN
    pagina home con vistas about,contacto,servicios

---
+++ b/Controllers/ServicesController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace b.Controllers
+{
+    public class ServicesController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
